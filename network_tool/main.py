4709aa30d333efcd34d28ab66f27f367318ba096
Yehuda Freiman <yehfre@icloud.com>
2025-12-11 10:31:18 +0200

    Implement subnet information collection and export
    This script collects subnet information and exports it to a text file.

---
+++ network_tool/main.py
@@ -0,0 +1,27 @@
+from network_tool.core.utils import *
+from network_tool.core.output_string import *
+
+
+if __name__ == "__main__":
+
+    ip_str = input_ip()
+    mask_str = input_subnet_mask()
+    class_type = identify_class_type()
+    network_address = calculate_network_address()
+    broadcast_address = calculate_broadcast_address()
+    num_hosts = calculate_num_hosts()
+    cidr = calculate_cidr_mask()
+
+    def export_to_txt():
+        with open(f"subnet_info_{ip_str}_205368319.txt", "w") as f:
+
+            f.write(format_input_ip(ip_str))
+            f.write(format_subnet_mask(mask_str))
+            f.write(format_classful_status(class_type))
+            f.write(format_network_address(network_address))
+            f.write(format_broadcast_address(broadcast_address))
+            f.write(format_num_hosts(num_hosts))
+            f.write(format_cidr_mask(cidr))
+
+
+    export_to_txt()
