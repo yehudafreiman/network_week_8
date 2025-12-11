9deb0018c58634f1dbc25a7c328700f5429c8bcc
Yehuda Freiman <yehfre@icloud.com>
2025-12-11 10:31:55 +0200

    Update utils.py

---
+++ network_tool/core/utils.py
@@ -0,0 +1,34 @@
+
+
+def input_ip():
+    ip = input("enter ip address: ")
+    return ip
+
+def input_subnet_mask():
+    subnet_mask = input("enter subnet mask: ")
+    return subnet_mask
+
+# def validate_ip(ip):
+#     return
+#
+# def validate_subnet_mask(subnet_mask):
+#     return
+
+def identify_class_type():
+    return
+
+def calculate_network_address():
+    return
+
+def calculate_broadcast_address():
+    return
+
+def calculate_num_hosts():
+    return
+
+def calculate_cidr_mask():
+    return
+
+
+
+
